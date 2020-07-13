5fa7c507c3374105d330ea059a1a717f5f5f4630
felipelealdefaria <felipelealdefaria@gmail.com>
2020-07-13 19:39:28 -0300

    task: fix input names to preview credit card

--- src/components/FormSteps/FormPayment/Methods/CreditCardForm/CreditCardForm.js
+++ src/components/FormSteps/FormPayment/Methods/CreditCardForm/CreditCardForm.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { BasicInput } from '../../../Inputs'
 import { schema } from '../../../../../utils'
-import { Form, FlexInput } from './CreditCardForm.styles'
+import { Form, FlexInput, Container, TitleForm } from './CreditCardForm.styles'
 
 export default function CreditCardForm() {
   const { errors, register } = useForm({
@@ -29,69 +29,72 @@ export default function CreditCardForm() {
   }
 
   return (
-    <Form>
-      <Col span={12}>
-        <BasicInput
-          type="text"
-          name="name"
-          errors={errors}
-          value={card.name}
-          register={register}
-          onFocus={handleInputFocus}
-          onChange={handleInputChange}
-          placeholder="LUIS F L FARIA"
-          label="Nome impresso no cartão"
-        />
-
-        <BasicInput
-          name="number"
-          type="tel"
-          errors={errors}
-          value={card.number}
-          register={register}
-          label="Número do cartão"
-          onFocus={handleInputFocus}
-          onChange={handleInputChange}
-          placeholder="0000 0000 0000 0000"
-        />
-
-        <FlexInput>
+    <Container>
+      <TitleForm>Dados do cartão:</TitleForm>
+      <Form>
+        <Col span={12}>
           <BasicInput
-            width={50}
-            type="tel"
-            name="expiry"
+            type="text"
+            name="name"
             errors={errors}
-            placeholder="00/00"
-            value={card.expiry}
-            register={register}
-            label="Validade do Cartão"
-            onFocus={handleInputFocus}
-            onChange={handleInputChange}
-          />
-
-          <BasicInput
-            width={50}
-            name="cvc"
-            type="tel"
-            label="CVC"
-            errors={errors}
-            value={card.cvc}
-            placeholder="XXX"
+            value={card.name}
             register={register}
             onFocus={handleInputFocus}
             onChange={handleInputChange}
+            placeholder="LUIS F L FARIA"
+            label="Nome impresso no cartão"
           />
-        </FlexInput>
-      </Col>
-      <Col span={12}>
-        <Cards
-          cvc={card.cvc}
-          name={card.name}
-          expiry={card.expiry}
-          focused={card.focus}
-          number={card.number}
-        />
-      </Col>
-    </Form>
+
+          <BasicInput
+            name="number"
+            type="tel"
+            errors={errors}
+            value={card.number}
+            register={register}
+            label="Número do cartão"
+            onFocus={handleInputFocus}
+            onChange={handleInputChange}
+            placeholder="0000 0000 0000 0000"
+          />
+
+          <FlexInput>
+            <BasicInput
+              width={50}
+              type="tel"
+              name="expiry"
+              errors={errors}
+              placeholder="00/00"
+              value={card.expiry}
+              register={register}
+              label="Validade do Cartão"
+              onFocus={handleInputFocus}
+              onChange={handleInputChange}
+            />
+
+            <BasicInput
+              width={50}
+              name="cvc"
+              type="tel"
+              label="CVC"
+              errors={errors}
+              value={card.cvc}
+              placeholder="XXX"
+              register={register}
+              onFocus={handleInputFocus}
+              onChange={handleInputChange}
+            />
+          </FlexInput>
+        </Col>
+        <Col span={12}>
+          <Cards
+            cvc={card.cvc}
+            name={card.name}
+            expiry={card.expiry}
+            focused={card.focus}
+            number={card.number}
+          />
+        </Col>
+      </Form>
+    </Container>
   )
 }
--- src/components/FormSteps/FormPayment/Methods/CreditCardForm/CreditCardForm.styles.js
+++ src/components/FormSteps/FormPayment/Methods/CreditCardForm/CreditCardForm.styles.js
@@ -7,8 +7,17 @@ export const Form = styled.form`
   text-align: center;
   align-items: center;
   border-radius: 20px;
-  margin: 40px 0px 0px 0px;
+  /* margin: 40px 0px 0px 0px; */
   border: 1px solid #e2e2e2;
+`
+
+export const Container = styled.div`
+  width: 100%;
+  margin: 40px 0px 0px 0px;
+`
+
+export const TitleForm = styled.p`
+  margin: 0px 0px 5px 20px;
 `
 
 export const FlexInput = styled.div`
